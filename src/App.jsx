19c7bf6b58cc6840c4137c5ddb7ecefb5f836a7e
Steno-95 <lezzistefano95@gmail.com>
2025-06-24 19:55:38 +0200

    Feat: add all the section components to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Hero from "./components/hero/Hero";
+import Popular from "./components/popular/Popular";
+import News from "./components/news/News";
+import Navigation from "./components/ui/Navigation";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Navigation />
+      <main className="wrapper">
+        <Hero />
+        <News />
+        <Popular />
+      </main>
+    </>
+  );
 }
 
 export default App;
