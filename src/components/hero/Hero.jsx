2bd30b5b5b22673ff65be67dd765f34d220d62eb
Steno-95 <lezzistefano95@gmail.com>
2025-06-25 19:43:10 +0200

    Feat: adjust layout and implement focus and hover state

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section className="hero">
-      <picture className="w-full">
+      <picture className="w-full md:col-span-2">
         <source
           srcSet="/images/image-web-3-desktop.jpg"
           media="(min-width: 1024px)"
@@ -9,9 +9,10 @@ function Hero() {
         <img
           src="/images/image-web-3-mobile.jpg"
           alt="picture of a puzzle of many colors"
+          className="w-full"
         />
       </picture>
-      <h1 className="text-(--darkest-blue) font-extrabold">
+      <h1 className="text-(--darkest-blue) font-extrabold md:row-span-2 md:leading-18">
         The Bright Future of Web 3.0?
       </h1>
       <p className="">
@@ -19,7 +20,7 @@ function Hero() {
         of the platforms back into the hands of the people. But is it really
         fulfilling its promise?
       </p>
-      <button className="bg-(--soft-red) w-fit px-8 py-2 font-bold tracking-[.2em]">
+      <button className="bg-(--soft-red) w-fit px-8 py-2 font-bold tracking-[.2em] text-[clamp(0.94em,_0.4em_+_2.5vw,_1.5em)] hover:bg-(--darkest-blue) hover:text-(--off-white) cursor-pointer focus:border-none focus:outline-none focus:bg-(--darkest-blue) focus:text-(--off-white) hover:font-normal focus:font-normal">
         READ MORE
       </button>
     </section>
